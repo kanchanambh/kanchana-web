1e77415a420416f6719ee83468508c6db8741bc1
kanchanambh <kanchanambandaraherath@gmail.com>
2023-08-08 01:19:28 +0530

    sitem 22

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,22 @@
 export default async function sitemap() {
   const baseUrl = process.env.BASE_URL;
 
+  const Portres = await fetch(process.env.BASE_URL + "/api/portfolios");
+  const repoPort = await Portres.json();
+
+  const postsUrls =
+    repoPort?.map((post) => {
+      return {
+        url: `${process.env.BASE_URL}/portfolios/${post.slug}`,
+        lastModified: new Date(),
+      };
+    }) ?? [];
+
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
+    ...postsUrls,
   ];
 }
